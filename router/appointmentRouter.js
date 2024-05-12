b4204b03244b0bdc372380f6ebb29163f29aceec
Kirill Kuts <p0limashine@gmail.com>
2024-05-12 23:58:47 +0200

    done with appointments logic

---
+++ router/appointmentRouter.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+    getAllAppointments,
+    postAppointment,
+    updateAppointment,
+    deleteAppointment,
+} from "../controller/appointmentController.js";
+import {
+    isAdminAuthenticated,
+    isPatientAuthenticated,
+} from "../middlewares/auth.js";
+
+const router = express.Router();
+
+router.post("/post", isPatientAuthenticated, postAppointment);
+router.get("/getall", isAdminAuthenticated, getAllAppointments);
+router.put("/update/:id", isAdminAuthenticated, updateAppointment);
+router.delete("/delete/:id", isAdminAuthenticated, deleteAppointment);
+
+export default router;
